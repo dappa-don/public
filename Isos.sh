4c621d10b8d67e60abceaae9a2411ec746991838
Dappa Don <43168651+dappa-don@users.noreply.github.com>
2021-10-03 02:56:54 +0100

    Update Isos.sh

--- Isos.sh
+++ Isos.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 PATH=/usr/bin:/usr/sbin
 
-wget 
-wget
+# hirens boot CD
+wget https://www.hirensbootcd.org/files/HBCD_PE_x64.iso
+# ALT rescue
+wget http://ftp.altlinux.org/pub/distributions/ALTLinux/p9/images/workstation/x86_64/alt-workstation-9.2-x86_64.iso
+# 
 wget
 wget
