4a72f1ee303cb8a115a39b22523d23a2e2b2ee3d
Dappa Don <43168651+dappa-don@users.noreply.github.com>
2021-10-03 03:18:02 +0100

    Update Isos.sh

--- Isos.sh
+++ Isos.sh
@@ -5,6 +5,7 @@ PATH=/usr/bin:/usr/sbin
 wget https://www.hirensbootcd.org/files/HBCD_PE_x64.iso
 # ALT rescue
 wget http://ftp.altlinux.org/pub/distributions/ALTLinux/p9/images/workstation/x86_64/alt-workstation-9.2-x86_64.iso
-# 
-wget
-wget
+# debian 11 non free
+wget https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current/amd64/iso-dvd/firmware-11.0.0-amd64-DVD-1.iso
+# debian 11 cinnamon live
+wget 
