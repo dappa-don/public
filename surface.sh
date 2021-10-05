82495dcbff781f22dc2b7bda2615ecc782c5e3a1
Dappa Don <43168651+dappa-don@users.noreply.github.com>
2021-10-05 01:24:22 +0100

    Update surface.sh

--- surface.sh
+++ surface.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-wget
+# recovry image
+wget http://e.downloads.prss.microsoft.com/db/SurfacePro5_BMR_155_12.343.2.zip?t=96215527-8bc4-4c91-b046-af524ef5338c&e=1633401612&h=7305c390548a9a88706b67a3b3b1339570a5c1674d6dfe776d698b5772119eab
 
+# drivers
+wget https://download.microsoft.com/download/4/C/7/4C7DA85D-03DE-4B89-B8E0-386
 
-https://www.microsoft.com/en-us/download/confirmation.aspx?id=46703
+#wget https://www.microsoft.com/en-us/download/confirmation.aspx?id=46703wget
