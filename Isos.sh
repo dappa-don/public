461b50ef1368dd613ceed0ca1dd8dfab0f7f63dd
Dappa Don <43168651+dappa-don@users.noreply.github.com>
2021-10-05 01:24:28 +0100

    Update Isos.sh

--- Isos.sh
+++ Isos.sh
@@ -3,12 +3,17 @@ PATH=/usr/bin:/usr/sbin
 
 # hirens boot CD
 wget https://www.hirensbootcd.org/files/HBCD_PE_x64.iso
-# ALT rescue
-wget http://ftp.altlinux.org/pub/distributions/ALTLinux/p9/images/workstation/x86_64/alt-workstation-9.2-x86_64.iso
 # debian 11 non free
 wget https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current/amd64/iso-dvd/firmware-11.0.0-amd64-DVD-1.iso
 # debian 11 cinnamon live
 wget https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current-live/amd64/iso-hybrid/debian-live-11.0.0-amd64-cinnamon+nonfree.iso
+# 
+https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current-live/amd64/iso-hybrid/debian-live-11.0.0-amd64-kde+nonfree.iso
+# debian net
+https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-11.0.0-amd64-netinst.iso
+# arch net boot executable
+https://archlinux.org/static/netboot/ipxe-arch.16e24bec1a7c.efi
+
 # windoze 10
 
 # ubuntu20.04 desktop
@@ -17,5 +22,17 @@ wget https://releases.ubuntu.com/20.04.3/ubuntu-20.04.3-desktop-amd64.iso
 wget ftp://ftp.rnl.tecnico.ulisboa.pt/pub/UBCD/ubcd539.iso
 # system rescue
 wget https://sourceforge.net/projects/systemrescuecd/files/sysresccd-x86/8.05/systemrescue-8.05-amd64.iso/download
- 
+# alt rescue
+http://nightly.altlinux.org/sisyphus/tested/regular-rescue-latest-x86_64.iso
 
+# teams windoze
+https://go.microsoft.com/fwlink/p/?LinkID=869426&clcid=0x809&culture=en-gb&country=GB&lm=deeplink&lmsrc=groupChatMarketingPageWeb&cmpid=directDownloadWin64
+# teams linux
+
+# zoom windows
+https://zoom.us/client/5.8.0.1324/ZoomInstaller.exe?archType=x64
+# zoom linux
+https://zoom.us/client/latest/zoom_amd64.deb
+# vscode windoze
+
+#vscode linux
